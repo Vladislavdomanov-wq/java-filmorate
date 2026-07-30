e23582777778b1919eb875cd096f2d9c338a0556
VlaDosick <Mlgtrack@yandex.ru>
2026-07-31 01:44:40 +0300

    обновил классы User и Film - добавил поля создал класс Friendship

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 public class Film {
@@ -11,4 +12,6 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private Integer duration;
+    private Set<String> genres;
+    private String mpaRating;
 }
